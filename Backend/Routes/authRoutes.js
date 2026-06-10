17518889ac48b1d56adcb6ed0dd2ea361e61d181
Chandu Yadav <chanduyadav24042009@gmail.com>
2026-06-10 21:16:17 +0530

    Middleware Added for users

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { registerUser, loginUser, getAllUsers} from "../Controller/authController.js";
+import admin from "../Middleware/adminMiddleware.js";
+import auth from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
-router.get("/fetch-users", getAllUsers)
+router.get("/fetch-users",admin, auth , getAllUsers)
+
 export default router;
