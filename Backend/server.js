db4098316714a18a07f02496a2e9ec905bc379df
Chandu Yadav <chanduyadav24042009@gmail.com>
2026-06-11 00:50:05 +0530

    add middleware url

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./Config/db.js";
 import authRoutes from "./Routes/authRoutes.js";
+import productRoutes from "./Routes/productRoutes.js";
 
 dotenv.config()
 connectDB()
 const app = express()
 app.use(express.json())
+app.use(express.urlencoded({extended : false}))
 const Port = process.env.PORT
 // app.use(cors());
 
@@ -15,6 +17,7 @@ app.get('/', (req, res)=>{
 })
 
 app.use('/api/auth', authRoutes)
+app.use('/api/product', productRoutes)
 
 app.listen(Port, ()=>{
     console.log(`Server is running on Port ${Port}`)
