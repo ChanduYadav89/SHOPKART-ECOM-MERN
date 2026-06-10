5bff00697e737cf88ce414dcd2021ba2c347eed6
Chandu Yadav <chanduyadav24042009@gmail.com>
2026-06-11 00:50:58 +0530

    fix errors

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { registerUser, loginUser, getAllUsers} from "../Controller/authController.js";
 import admin from "../Middleware/adminMiddleware.js";
-import auth from "../Middleware/authMiddleware.js";
+import protect from "../Middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser)
 router.post("/login", loginUser)
-router.get("/fetch-users",admin, auth , getAllUsers)
+router.get("/fetch-users",protect, admin, getAllUsers)
 
 export default router;
